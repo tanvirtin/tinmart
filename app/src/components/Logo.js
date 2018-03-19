eb17729682b624ef93ed4e641735e631b9999640
tanvirtin <tanvir.tinz@gmail.com>
2018-03-19 03:52:43 -0400

    adding animation to the logo

--- app/src/components/Logo.js
+++ app/src/components/Logo.js
@@ -1,18 +1,29 @@
 // Dumb component
 
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { 
+    Text,
+    Animated
+} from 'react-native';
 
 // contains basic information about the app
 const appInfo = require('../../appInfo.json');
 
 export const Logo = (props) => {
     // checks if a style prop is given to the component
-    const isUndefined = props.style === undefined ? true : false;
+    const propsIsUndefined = props.style === undefined ? true : false;
 
-    // if a style prop is given then I render the text component with the given style component otherwise I use my defaulty style component
-    return isUndefined ? (<Text style = {styles.logo}> {appInfo.name} </Text>) : (<Text style = {props.style}> {appInfo.name} </Text>);
+    // jsx element stored in a variable for conditional rendering in return statement
+    const textWithoutProp = <Text style = {styles.logo}> {appInfo.name} </Text>;
+    // jsx element stored in a variable for conditional rendering in return statement
+    const textWithProp = <Text style = {props.style}> {appInfo.name} </Text>;
 
+    return (
+        <Animated.View>
+            {/*if a style prop is given then I render the text component with the given style component otherwise I use my defaulty style component*/}
+            {propsIsUndefined ? textWithoutProp : textWithProp}
+        </Animated.View>
+    );
 }
 
 // Stylesheet.create() must not be used as a plain JavaScript object is not returned and for performance optimization
