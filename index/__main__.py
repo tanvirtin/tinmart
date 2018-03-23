5483cbf16aa01df351e0d50ce396d542044b8481
tanvirtin <tanvir.tinz@gmail.com>
2018-03-22 22:51:56 -0400

    concurrency added to indexing documents using threads

--- index/__main__.py
+++ index/__main__.py
@@ -2,6 +2,7 @@ from indexer import IndexController
 
 def main():
     indexer = IndexController()
+    indexer.delete_index()
     indexer.index_crawled_documents()
 
 if __name__ == '__main__':
--- index/indexer.py
+++ index/indexer.py
@@ -10,7 +10,8 @@ class IndexController(object):
     def __init__(self):
         # when the object is instantiated connect to the walmartcrawler database in mongodb
         connect('walmartcrawler')
-        self.elasticsearchcli = ElasticSearchCli('tinmart')
+        self.index_name = 'tinmart'
+        self.elasticsearchcli = ElasticSearchCli(self.index_name)
 
     def __create_lucene_dict(self, crawler_document):
         return {
@@ -24,11 +25,29 @@ class IndexController(object):
             'tags': crawler_document.tags
         }
 
+
+    def __add_document(self, crawler_document):
+        lucene_document = self.__create_lucene_dict(crawler_document)
+        docId = str(lucene_document['docId'])
+        status = self.elasticsearchcli.index_document('products', docId, lucene_document)
+        
+        if status:
+            print('Document -> {} indexed...'.format(docId))
+  
     '''
-        indexes all the documents in mongodb in a multithreaded fashion
+        Indexes all the documents in mongodb in a multithreaded fashion
     '''
     def index_crawled_documents(self):
         crawler_documents = CrawlerDocument.objects
         for crawler_document in crawler_documents:
-            lucene_document = self.__create_lucene_dict(crawler_document)
-            self.elasticsearchcli.index_document(lucene_document)
+            # create the thread passing in the method which indexes lucene
+            thread = Thread(target = self.__add_document, args = (crawler_document, ))
+            thread.start()
+        
+
+    '''
+        Deletes an index from the database
+    '''
+    def delete_index(self):
+        print('Index {} removed...'.format(self.index_name))
+        status = self.elasticsearchcli.delete_index()
